545d534ea993d142aff3a7983093184586a56933
PazitoPazos <javier.pazos001@gmail.com>
2024-05-01 19:52:43 +0200

    Removed account link from navbar

--- src/components/NavbarTop.tsx
+++ src/components/NavbarTop.tsx
@@ -59,14 +59,6 @@ function NavbarTop() {
             {session ? (
               <ul className="py-1">
                 <li>
-                  <Link
-                    href="/account"
-                    className="block px-4 py-2 text-xl text-gray-800 hover:bg-indigo-100"
-                  >
-                    Account
-                  </Link>
-                </li>
-                <li>
                   <button
                     id="btnLogout"
                     className="block w-full px-4 py-2 text-center text-xl text-gray-800 hover:bg-indigo-100"
