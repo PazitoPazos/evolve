32a0df1f2c9bf4d69ab6d0134c811bcf22ca3022
PazitoPazos <javier.pazos001@gmail.com>
2024-05-25 12:35:42 +0200

    Add sharp package for image optimization on production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -140,7 +140,7 @@ function Features() {
                 onClick={handleClick}
                 data-id-feature={index}
                 className={`flex items-center justify-center rounded-lg p-4 text-center text-xl text-accent transition-all duration-700 hover:cursor-pointer lg:text-2xl ${
-                  currentFeatureIndex === index ? `bg-primary-light` : ''
+                  currentFeatureIndex === index ? 'bg-primary-light' : ''
                 }`}
               >
                 {feature.icon}
